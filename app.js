2816003693537560d37f12af534043ff6a9a6402
Tony <incorvaia.tony@gmail.com>
2025-01-29 20:59:35 +0000

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello TP3 hihi\n');
+    res.end('Hello TP3 hihi \n');
 });
 server.listen(port, hostname, () => {
     console.log('Server running at http://%s:%s/', hostname, port);
