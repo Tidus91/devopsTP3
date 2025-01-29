43e03b584e913a9c1449273cc4b8b2ee8c14d8f0
Tony <incorvaia.tony@gmail.com>
2025-01-29 19:48:07 +0000

    change app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello TP3\n');
+    res.end('Hello TP3 hihi\n');
 });
 server.listen(port, hostname, () => {
     console.log('Server running at http://%s:%s/', hostname, port);
