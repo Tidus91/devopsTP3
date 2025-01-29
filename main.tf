393e43fc4bd3dd528d2d9ccd3b71b3cda4a70345
Tony <incorvaia.tony@gmail.com>
2025-01-29 17:24:19 +0000

    fix main.tf

--- main.tf
+++ main.tf
@@ -1,61 +1,32 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-# 🔹 Activer les APIs nécessaires
-resource "google_project_service" "enable_services" {
-  for_each = toset([
-    "run.googleapis.com",             # Cloud Run
-    "artifactregistry.googleapis.com", # Artifact Registry
-    "cloudbuild.googleapis.com",       # Cloud Build
-  ])
-  service = each.key
-}
-
-
 resource "google_project_service" "run" {
   service = "run.googleapis.com"
 }
-
-
 
 resource "google_project_service" "artifactregistry" {
   service = "artifactregistry.googleapis.com"
 }
 
-
-
-# 🔹 Créer un repository Artifact Registry pour stocker l’image Docker
-resource "google_artifact_registry_repository" "node_repo" {
+resource "google_artifact_registry_repository" "my-repo" {
   location      = var.region
-  repository_id = "node-app-repo"
+  repository_id = "my-nodejs-repo"
   format        = "DOCKER"
 
   depends_on = [google_project_service.artifactregistry]
 }
 
-# 🔹 (Bonus) Déployer l’application sur Cloud Run
-resource "google_cloud_run_service" "node_app" {
-  name     = "node-app"
+resource "google_cloud_run_service" "my-service" {
+  name     = "my-nodejs-app"
   location = var.region
 
   template {
     spec {
       containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/node-app-repo/node-app:latest"
-        ports {
-          container_port = 80
-        }
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.my-repo.repository_id}/my-nodejs-app:latest"
       }
     }
   }
@@ -68,9 +39,6 @@ resource "google_cloud_run_service" "node_app" {
   depends_on = [google_project_service.run]
 }
 
-# 🔹 Donner les permissions à Cloud Build pour déployer sur Cloud Run
-resource "google_project_iam_member" "cloud_build_run" {
-  project = var.project_id
-  role    = "roles/run.admin"
-  member  = "serviceAccount:${var.project_id}@cloudbuild.gserviceaccount.com"
+output "cloud_run_url" {
+  value = google_cloud_run_service.my-service.status[0].url
 }
